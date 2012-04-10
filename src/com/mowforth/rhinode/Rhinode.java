561b963f4b33a1aa005381b6d699f7773f8f2b68
Chris Mowforth <chris@mowforth.com>
2012-04-10 13:41:53 +0100

    rm external rhino dep

--- src/com/mowforth/rhinode/Rhinode.java
+++ src/com/mowforth/rhinode/Rhinode.java
@@ -1,7 +1,12 @@
-import org.mozilla.javascript.*;
+import java.io.Console;
 
 public class Rhinode {
     public static void main(String[] args) {
-        System.out.println(args.length);
+        if (args.length == 0) {
+            System.out.println("repl");
+            
+        } else {
+            System.out.println("parse file");
+        }
     }
 }
