38ce7d1effd95e238bac02ba11d360d5ba253aeb
Chris Mowforth <chris@mowforth.com>
2012-04-11 17:44:35 +0100

    add resources to build

---
+++ resources/bootstrap.js
@@ -0,0 +1,4 @@
+var require = function (name) {
+  
+}
+
--- src/com/mowforth/rhinode/Rhinode.java
+++ src/com/mowforth/rhinode/Rhinode.java
@@ -1,12 +1,28 @@
-import java.io.Console;
+package com.mowforth.rhinode;
+
+import javax.script.*;
+import java.nio.file.*;
+import java.io.IOException;
 
 public class Rhinode {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("repl");
-            
-        } else {
-            System.out.println("parse file");
-        }
+    
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+
+        String source = readFile("test.js");
+        try {
+            engine.eval(source);
+        } catch (ScriptException e) {}
+
+        System.exit(0);
+    }
+
+    public static String readFile(String file) {
+        Path path = FileSystems.getDefault().getPath(file);
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            return new String(bytes);
+        } catch (IOException e) { return ""; }
     }
 }
