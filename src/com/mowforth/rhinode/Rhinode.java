97b183f46cb5c2378fe023f2cedc11cc50b8b4a6
Chris Mowforth <chris@mowforth.com>
2012-04-14 15:56:43 +0100

    add module loader and bootstrap file

--- src/com/mowforth/rhinode/Rhinode.java
+++ src/com/mowforth/rhinode/Rhinode.java
@@ -1,28 +1,24 @@
 package com.mowforth.rhinode;
 
 import javax.script.*;
-import java.nio.file.*;
-import java.io.IOException;
 
 public class Rhinode {
+
+    private static ScriptEngine engine;
+
     public static void main(String[] args) {
     
         ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("JavaScript");
+        engine = factory.getEngineByName("JavaScript");
 
-        String source = readFile("test.js");
-        try {
-            engine.eval(source);
-        } catch (ScriptException e) {}
+        ModuleLoader.require("bootstrap", engine);
+        ModuleLoader.require("os", engine);
 
         System.exit(0);
     }
 
-    public static String readFile(String file) {
-        Path path = FileSystems.getDefault().getPath(file);
-        try {
-            byte[] bytes = Files.readAllBytes(path);
-            return new String(bytes);
-        } catch (IOException e) { return ""; }
+    public static ScriptEngine getEngine() {
+        return engine;
     }
+
 }
