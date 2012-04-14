47b886cb2b6ebc66b1fce79bcbb7a965c73950e2
Chris Mowforth <chris@mowforth.com>
2012-04-14 20:28:59 +0100

    shutdown notice

--- src/com/mowforth/rhinode/REPL.java
+++ src/com/mowforth/rhinode/REPL.java
@@ -9,19 +9,35 @@ class REPL {
 
     private ConsoleReader console;
     private ScriptEngine engine;
+    private int parseDepth;
 
     public REPL(ScriptEngine e) {
         try {
             console = new ConsoleReader();
-            console.setDefaultPrompt(">");
-        } catch (IOException x) {}
+        } catch (IOException x) {
+            System.out.println("Couldn't open console.");
+            System.exit(1);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    System.out.println("\nCaught ^C, shutting down...");
+                }
+        });
+        
         engine = e;
+        parseDepth = 0;
     }
 
     public void start() {
-        String input;
+        String input, prompt;
         try {
-            while ((input = console.readLine()) != null) {
+            while (true) {
+                prompt = ">";
+                if (parseDepth > 0) prompt = "...";
+
+                input = console.readLine(prompt);
+
                 try {
                     Object out = engine.eval(input);
                     System.out.print(out);
@@ -37,8 +53,9 @@ class REPL {
     }
 
     private int occurrencesOf(String source, String pattern) {
-        String[] chars = source.split("");
-        return 1;
+        String[] chars = source.split(""); int num = 0;
+        for (String c : chars) { if (c.equals(pattern)) num++; }
+        return num;
     }
     
 }
