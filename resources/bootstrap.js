121e3528c1ca622be798df0a212765de8ac813f8
Chris Mowforth <chris@mowforth.com>
2012-04-14 18:36:59 +0100

    added simple JSON parser

--- resources/bootstrap.js
+++ resources/bootstrap.js
@@ -1,6 +1,9 @@
+importPackage(com.mowforth.rhinode);
+
 var require = function (name) {
   var exports = {};
-  com.mowforth.rhinode.ModuleLoader.require(name, com.mowforth.rhinode.Rhinode.getEngine());
+  ModuleLoader.require(name,
+                       Rhinode.getEngine(), exports);
   return exports;
 }
 
