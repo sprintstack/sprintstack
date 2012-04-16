afb9025648a755c6b83c97d73508191c0db07e3b
Chris Mowforth <chris@mowforth.com>
2012-04-16 22:01:00 +0100

    some low-hanging OS fruit

--- resources/os.js
+++ resources/os.js
@@ -1,6 +1,28 @@
+importPackage(java.lang);
+
 var OS = function() {
   this.hostname = function() {
     return java.net.InetAddress.getLocalHost().getHostName();
+  }
+
+  this.type = function() {
+    return System.getProperty('os.name');
+  }
+
+  this.arch = function() {
+    return System.getProperty('os.arch');
+  }
+
+  this.release = function() {
+    return System.getProperty('os.version');
+  }
+
+  this.freemem = function() {
+    return Runtime.getRuntime().freeMemory();
+  }
+
+  this.totalmem = function() {
+    return Runtime.getRuntime().maxMemory();
   }
 };
 
