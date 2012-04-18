d64897d6db474c64131090c8a51f4293b4a67c97
Chris Mowforth <chris@mowforth.com>
2012-04-18 14:34:24 +0100

    add braindead arg parsing, move base easter-eggs into a module

--- src/com/mowforth/rhinode/Rhinode.java
+++ src/com/mowforth/rhinode/Rhinode.java
@@ -7,9 +7,13 @@ public class Rhinode {
     private static ScriptEngine engine;
 
     public static void main(String[] args) {
-
-        REPL r = new REPL(Environment.getDefaultEngine());
-        r.start();
+        if (args.length == 0) {
+            REPL r = new REPL(Environment.getDefaultEngine());
+            r.start();
+        } else {
+            String scriptName = args[args.length-1];
+            ModuleLoader.require(scriptName);
+        }
 
         System.exit(0);
     }
