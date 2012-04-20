d2734dae321b18950bc759c11498c3caa18e82a3
Chris Mowforth <chris@mowforth.com>
2012-04-20 15:03:30 +0100

    path module

---
+++ resources/path.js
@@ -0,0 +1,29 @@
+importClass(com.mowforth.rhinode.core.IPath);
+
+var Path = function() {
+
+  this.normalize = function(p) {
+    return IPath.normalize(p).toString();
+  }
+
+  this.join = function() {
+    var args = Array.prototype.slice.call(arguments);
+    return IPath.join(args).toString();
+  }
+
+  this.resolve = function() {
+    
+  }
+
+  this.relative = function(from, to) {
+    return IPath.relativize(from, to).toString();
+  }
+
+  this.existsSync = function(p) {
+    return IPath.exists(p);
+  }
+
+}
+
+module.exports = new Path();
+
