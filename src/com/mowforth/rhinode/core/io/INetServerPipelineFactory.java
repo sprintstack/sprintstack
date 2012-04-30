261c9b2d9e23caf76a038fe3709e7c0343f5500a
Chris Mowforth <chris@mowforth.com>
2012-04-30 13:05:33 +0100

    default do string-based framing

--- src/com/mowforth/rhinode/core/io/INetServerPipelineFactory.java
+++ src/com/mowforth/rhinode/core/io/INetServerPipelineFactory.java
@@ -4,11 +4,21 @@ import static org.jboss.netty.channel.Channels.*;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
+import org.jboss.netty.handler.codec.frame.Delimiters;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
 
 public class INetServerPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() {
         ChannelPipeline pipeline = pipeline();
+
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192,
+                                                                  Delimiters.lineDelimiter()));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+
         pipeline.addLast("handler", new INetServerHandler());
         return pipeline;
     }
