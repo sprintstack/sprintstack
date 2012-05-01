0ca4de8c1f1b711eaaebf409d0ddf319f3e7699a
Chris Mowforth <chris@mowforth.com>
2012-05-01 23:36:33 +0100

    stop actor on connection close, add postStop hook

--- src/com/mowforth/rhinode/core/io/INetActor.java
+++ src/com/mowforth/rhinode/core/io/INetActor.java
@@ -3,10 +3,14 @@ package com.mowforth.rhinode.core.io;
 import akka.actor.UntypedActor;
 import com.mowforth.rhinode.dispatch.IFunction;
 import org.jboss.netty.channel.MessageEvent;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class INetActor extends UntypedActor {
 
+    private ConcurrentHashMap messageHandlers;
+
     public void onReceive(Object message) {
+        if (messageHandlers == null) messageHandlers = new ConcurrentHashMap();
         if (message instanceof String) {
             System.out.println(message);
         } else if (message instanceof MessageEvent) {
@@ -14,6 +18,10 @@ public class INetActor extends UntypedActor {
             System.out.println(e.getMessage());
             e.getChannel().write(e.getMessage());
         }
+    }
+
+    public void postStop() {
+        System.out.println("stopped...");
     }
 
 }
