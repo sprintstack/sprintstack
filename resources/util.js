51ec891fa0a941c846435f52f90b7a6dcde22305
Chris Mowforth <chris@mowforth.com>
2012-05-02 18:06:47 +0100

    rudimentary printf

---
+++ resources/util.js
@@ -0,0 +1,29 @@
+function s (str) {
+  var i = 1, args = arguments;
+  return String(str).replace(/%?%(d|s|j)/g, function (symbol, type) {
+    if ('%' == symbol[1]) return symbol;
+      var arg = args[i++];
+      switch (type) {
+        case 'd': return Number(arg);
+        case 'j': return JSON.stringify(arg);
+      }
+      return String(arg);
+  });
+};
+
+s.extend = function () {
+  String.prototype.s = function () {
+    var arr = [this];
+    arr.push.apply(arr, arguments)
+    return s.apply(null, arr);
+  }
+};
+
+var Util = function() {
+
+  this.format = s
+
+}
+
+module.exports = new Util();
+
