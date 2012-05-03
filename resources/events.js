acebfef963461474a19e3fbfc26004319fc53944
Chris Mowforth <chris@mowforth.com>
2012-05-03 16:11:03 +0100

    exported EventEmitter

--- resources/events.js
+++ resources/events.js
@@ -23,3 +23,5 @@ var EventEmitter = function() {
 
 }
 
+exports.EventEmitter = EventEmitter;
+
--- server.js
+++
@@ -1,7 +0,0 @@
-importPackage(com.mowforth.rhinode.core.io);
-
-var server = new INetServer(8080);
-
-server.run();
-
-
