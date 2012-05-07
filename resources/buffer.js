01ff419e64c42d9e8d71e20f2a8df04facd88d97
Chris Mowforth <chris@mowforth.com>
2012-05-07 20:33:04 +0100

    buffer r/w

--- resources/buffer.js
+++ resources/buffer.js
@@ -1,11 +1,48 @@
 importClass(java.nio.ByteBuffer);
 
-var Buffer = function(ctor) {
-  if (ctor.constructor == Number) {
-//    this.internalBuffer = ByteBuffer.allocate(ctor);
-    console.log('num');
-    console.log(module.uri);
+var fun = require('match');
+
+var $ = fun.parameter;
+var _ = fun.wildcard;
+
+var Buffer = function() {
+  var self = this;
+
+  ctor = fun(
+    [[String], function(s) {
+      return new Buffer(s, 'UTF-8');
+    }],
+    [[String, String], function(s, enc) {
+      bytes = new java.lang.String(s).getBytes(enc);
+      return ByteBuffer.wrap(bytes);
+    }],
+    [[Number], function(n) {
+      return ByteBuffer.allocate(n);
+    }],
+    [[Array], function(a) {
+      return ByteBuffer.wrap(a);
+    }]
+  );
+
+  this.byteBuffer = ctor(Array.prototype.slice.call(arguments));
+
+  this.write = function() {
+    args = Array.prototype.slice.call(arguments);
+    writer = fun(
+      [[String], function(s) {
+        return self.write(s, 0, s.length, 'UTF-8');
+      }],
+      [[String, Number, Number, String], function(s, offset, length, enc) {
+        bytes = new java.lang.String(s).getBytes(enc);
+        self.byteBuffer.put(bytes, (offset || 0), bytes.length);
+        return true;
+      }]
+    );
+    writer(args);
   }
+
+  this.length = this.byteBuffer.capacity();
+
 };
 
 module.exports = Buffer;
