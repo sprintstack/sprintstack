4e1ae5a30ea1fe0d2b7207b5604864b5e7b0f71d
Chris Mowforth <chris@mowforth.com>
2012-05-17 23:58:12 +0300

    more tweaks to actor api; add live upgrade/downgrade

--- resources/actor.js
+++ resources/actor.js
@@ -13,26 +13,58 @@ var actorFactory = function(handler) {
 
 function merge(obj1, obj2) {
   for (attr in obj2) obj1[attr] = obj2[attr];
-}
+};
 
-var actor = function(handler) {
-  /*var base = {
-    spawn: function(fn) {
-      var af = actorFactory(fn);
-      return this.getContext().actorOf(new Props().withCreator(af));
+var actor = function(fn) {
+  var base = {
+    preStart: function() {
+      this.handlers = [fn];
+      this.handler = fn;
+    },
+    reply: function(msg) {
+      this.getSender().tell(msg);
+    },
+    onReceive: function(msg) {
+      if (msg.constructor === Array) {
+        switch (msg[0]) {
+          case "upgrade":
+          this.handlers.push(msg[1]);
+          break;
+          case "downgrade":
+          if (this.handlers.length > 1) {
+            this.handlers.pop();
+          }
+          break;
+        }
+      } else {
+        h = this.handlers[this.handlers.length-1];
+        h.call(this, msg);
+      }
     }
   };
-  merge(handler, base);*/
 
-  var af = actorFactory(handler);
+  var af = actorFactory(base);
   if (arguments.length == 2) {
     var options = arguments[1];
     actor = Dispatch.getSystem().actorOf(new Props().withCreator(af).withRouter(new RoundRobinRouter(options["n"])));
   } else {
     actor = Dispatch.getSystem().actorOf(new Props().withCreator(af));
   }
-  
-  return actor;
-};
+
+  this.actor = actor;
+
+  this.send = function(msg) {
+    this.actor.tell(msg);
+  }
+
+  this.upgrade = function(fn) {
+    this.send(['upgrade', fn]);
+  }
+
+  this.downgrade = function() {
+    this.send(['downgrade']);
+  }};
+
 
 module.exports = actor;
+
