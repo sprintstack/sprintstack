d1a0b6bf78b68ad69234136a2fdacbc017c9e388
Chris Mowforth <chris@mowforth.com>
2012-05-20 01:23:09 +0300

    process mod updates

--- resources/process.js
+++ resources/process.js
@@ -1,4 +1,35 @@
+importClass(java.lang.Runtime);
+importClass(java.lang.management.ManagementFactory);
+
+var os = require('os');
+
 var Process = function() {
+
+  var mx = ManagementFactory.getRuntimeMXBean();
+  var rt = Runtime.getRuntime();
+
+  this.platform = os.type();
+
+  this.arch = os.arch();
+
+  this.pid = java.lang.System.getProperty("sprintstack.pid");
+
+  this.nextTick = function(callback) {
+    return new future(callback);
+  }
+
+  this.uptime = function() {
+    return mx.getUptime();
+  }
+
+  this.memoryUsage = function() {
+    var heap = rt.totalMemory();
+    var used = heap - rt.freeMemory();
+    return {
+      heapTotal: heap,
+      heapUsed: used
+    };
+  }
 
   this.exit = function() {
     var code = 0;
