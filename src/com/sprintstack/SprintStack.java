6defda8a84fb3f7c83b6d6fe87a22a78564116a6
Chris Mowforth <chris@mowforth.com>
2012-05-21 02:29:28 +0300

    add cmd line option parser

--- src/com/sprintstack/SprintStack.java
+++ src/com/sprintstack/SprintStack.java
@@ -6,17 +6,13 @@ import org.mozilla.javascript.tools.shell.Main;
 public class SprintStack {
 
     public static void main(String[] args) {
-        Dispatch.setupSystem();
-
-        if (args.length == 1 && args[0].equals("-worker")) {
-            System.out.println("just a worker bee...");
-        } else {
-            Main.setBootstrap(ModuleLoader.require("module"));
-            Main.main(args);
-        }
+        Main.setBootstrap(ModuleLoader.require("module"));
+        Main.main(args);
 
         if (Dispatch.getAwait()) Dispatch.getSystem().awaitTermination();
         System.exit(0);
     }
+
+
 
 }
