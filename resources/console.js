2e234610daacde59bfc408c645a2f96c7846f276
Chris Mowforth <chris@mowforth.com>
2012-05-22 16:41:11 +0300

    custom logger impl, doesn't print ActorRef

--- resources/console.js
+++ resources/console.js
@@ -1,38 +1,57 @@
-importClass(com.sprintstack.dispatch.Dispatch);
+importClass(java.lang.System);
 
 var util = require('util');
 
+// Accepts log message in the format {"t":x,"p":y}
+// where t = type, p = payload
+var Logger = function() {
+
+  var internalLogger = new actor(function(msg) {
+    if (msg.t == "err") System.err.println(msg.p);
+    if (msg.t == "out") System.out.println(msg.p);
+  });
+
+  this.out = function(msg) {
+    internalLogger.send({"t":"out","p":msg});
+  }
+
+  this.err = function(msg) {
+    internalLogger.send({"t":"err","p":msg});
+  }
+
+}
+
 var Console = function() {
 
-  logger = Dispatch.getSystem().log();
+  logger = new Logger();
 
   timer = new actor(function(msg) {
     if (!this.timestamps) this.timestamps = {};
     if (msg.constructor === String) {
       this.timestamps[msg] = new Date().getTime();
-    java.lang.System.out.println(msg)
     } else {
       var id = this.timestamps[msg.end];
       if (id) {
         var endtime = new Date().getTime();
-        logger.info(msg.end + util.format(": %dms", endtime - id));
+        delete this.timestamps[msg.end];
+        logger.out(msg.end + util.format(": %dms", endtime - id));
       }
     }
   });
 
   this.log = function() {
-    logger.info(util.format.apply(null, arguments));
+    var str = util.format.apply(null, arguments);
+    logger.out(str);
   }
 
   this.info = this.log;
 
   this.warn = function() {
-    logger.warning(util.format.apply(null, arguments));
+    var str = util.format.apply(null, arguments);
+    logger.err(str);
   }
 
-  this.error = function() {
-    logger.error(util.format.apply(null, arguments));
-  }
+  this.error = this.warn;
 
   this.time = function(id) {
     timer.send(id);
