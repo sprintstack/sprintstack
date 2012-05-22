e4a8394257a661ff88ad09478a892a5eafdfe7c6
Chris Mowforth <chris@mowforth.com>
2012-05-22 22:55:34 +0300

    refactored ws echo server

--- resources/websocket.js
+++ resources/websocket.js
@@ -1,19 +1,10 @@
-importClass(java.net.InetSocketAddress);
-importClass(java.util.concurrent.Executors);
-importClass(Packages.org.jboss.netty.bootstrap.ServerBootstrap);
 importClass(Packages.org.jboss.netty.buffer.ChannelBuffers);
 importClass(Packages.org.jboss.netty.channel.Channels);
 importClass(Packages.org.jboss.netty.channel.ChannelFutureListener);
-importClass(Packages.org.jboss.netty.channel.ChannelPipelineFactory);
 importClass(Packages.org.jboss.netty.channel.MessageEvent);
 importClass(Packages.org.jboss.netty.channel.SimpleChannelUpstreamHandler);
-importClass(Packages.org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory);
-importClass(Packages.org.jboss.netty.handler.codec.http.HttpChunkAggregator);
-importClass(Packages.org.jboss.netty.handler.codec.http.HttpRequestDecoder);
-importClass(Packages.org.jboss.netty.handler.codec.http.HttpResponseEncoder);
 importClass(Packages.org.jboss.netty.handler.codec.http.HttpMethod);
 importClass(Packages.org.jboss.netty.handler.codec.http.HttpHeaders);
-importClass(Packages.org.jboss.netty.handler.codec.http.HttpHeaders.Names);
 importClass(Packages.org.jboss.netty.handler.codec.http.HttpResponseStatus);
 importClass(Packages.org.jboss.netty.handler.codec.http.HttpVersion);
 importClass(Packages.org.jboss.netty.handler.codec.http.HttpRequest)
@@ -26,106 +17,74 @@ importClass(Packages.org.jboss.netty.handler.codec.http.websocketx.TextWebSocket
 importClass(Packages.org.jboss.netty.handler.codec.http.websocketx.WebSocketServerHandshaker);
 importClass(Packages.org.jboss.netty.handler.codec.http.websocketx.WebSocketServerHandshakerFactory);
 
+var http = require('http');
+var match = require('match');
+var util = require('util');
 
-var Pipeline = function(connectionListener) {
-  return new JavaAdapter(ChannelPipelineFactory, {getPipeline: function() {
-    var pipeline = Channels.pipeline();
+var WsRequest = http.ServerRequest;
+var WsResponse = http.ServerResponse;
 
-    pipeline.addLast("decoder", new HttpRequestDecoder());
-    pipeline.addLast("aggregator", new HttpChunkAggregator(65536));
-    pipeline.addLast("encoder", new HttpResponseEncoder());
-    pipeline.addLast("handler", ServerHandler(connectionListener));
-    return pipeline;
-  }});
-}
-
-var worker = {
-  onReceive: function(msg) {
-    this.getSender().tell(['res', "Hello, world!"]);
-  }
-};
-
-var boss = {
-  onReceive: function(msg) {
-    if (msg[0] == 'data') {
-      this.channel = msg[2].getChannel();
-      slave.tell(['data', msg[2].getMessage()], this.getSelf());
-    } else if (msg[0] == 'err') {
-      console.log(msg[2]);
-    } else if (msg[0] == 'res') {
-      this.handleHttp(msg[1]);
-    }
-  },
-  handleHttp: function(msg) {
-    res = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-    res.setContent(ChannelBuffers.copiedBuffer("Hello, world!\n", "UTF-8"));
-    this.channel.write(res).addListener(new JavaAdapter(ChannelFutureListener, {operationComplete: function(f) {
-      f.getChannel().close();
-    }}));
-/*
+var WebSocketHandler = function(connectionListener) {
+  return new JavaAdapter(SimpleChannelUpstreamHandler, {
+    fired: new AtomicBoolean(false),
+    observers: [],
+    messageReceived: function(ctx, e) {
+      var msg = e.getMessage();
+      if (msg instanceof HttpRequest) {
+        this.handleHttp(ctx, msg);
+      } else if (msg instanceof WebSocketFrame) {
+        this.handleWs(ctx, msg);
+      }
+    },
+    channelClosed: function(ctx, msg) {
+      req.emit('end');
+    },
+    handleHttp: function(ctx, msg) {
       wsfactory = new WebSocketServerHandshakerFactory(this.getWebSocketLocation(msg), null, false);
       this.handshaker = wsfactory.newHandshaker(msg);
       if (this.handshaker == null) {
         wsFactory.sendUnsupportedWebSocketVersionResponse(ctx.getChannel());
       } else {
         this.handshaker.handshake(ctx.getChannel(), msg).addListener(WebSocketServerHandshaker.HANDSHAKE_LISTENER);
-      }*/
+      }
     },
-  handleWs: function(ctx, frame) {
-    console.log('ws');
-    if (frame instanceof CloseWebSocketFrame) {
-      this.handshaker.close(ctx.getChannel(), frame);
+    handleWs: function(ctx, frame) {
+      if (frame instanceof CloseWebSocketFrame) {
+        this.handshaker.close(ctx.getChannel(), frame);
       return;
-    } else if (frame instanceof PingWebSocketFrame) {
-      ctx.getChannel.write(new PongWebsocketFrame(frame.getBinaryData()))
-      return;
-    } else if (frame instanceof TextWebSocketFrame) {
-      req = frame.getText();
-      ctx.getChannel().write(new TextWebSocketFrame("bumeyes"));
-    }
-  },
-  getWebSocketLocation: function(msg) {
-    return "ws://" + msg.getHeader(HttpHeaders.Names.HOST) + "/ws";
-  }
-};
-
-var slave = new actor(worker, {"n":5});
-
-var ServerHandler = function(connectionListener) {
-  return new JavaAdapter(SimpleChannelUpstreamHandler, {
-    supervisor: new actor(boss),
-    channelOpen: function(ctx, e) {
-      this.supervisor.tell(['open', ctx, e]);
+      } else if (frame instanceof PingWebSocketFrame) {
+        ctx.getChannel.write(new PongWebsocketFrame(frame.getBinaryData()))
+        return;
+      } else if (frame instanceof TextWebSocketFrame) {
+        msg = frame.getText();
+        ctx.getChannel().write(new TextWebSocketFrame(msg));
+      }
     },
-    messageReceived: function(ctx, e) {
-      this.supervisor.tell(['data', ctx, e]);
+    getWebSocketLocation: function(msg) {
+      return "ws://" + msg.getHeader(HttpHeaders.Names.HOST) + "/ws";
     },
     exceptionCaught: function(ctx, e) {
-      this.supervisor.tell(['err', ctx, e]);
+      e.getChannel().close();
     }
   });
 };
 
-var Server = function(connectionListener) {
+var Server = http.Server;
 
-  var internalAddress = null;
+exports.createServer = function(options, listener) {
+  var opt, fn;
+  var args = match(
+    [[Function], function(f) {
+      [opt, fn] = [{}, f];
+    }],
+    [[Object, Function], function(o, f) {
+      [opt, fn] = [o, f];
+    }]
+  );
 
-  this.listen = function(port, cb) {
-    new future(function() {
-      internalAddress = new InetSocketAddress(port);
-      var factory = new NioServerSocketChannelFactory(Executors.newSingleThreadExecutor(),
-                                                      Executors.newCachedThreadPool(), 1);
-      var bootstrap = new ServerBootstrap(factory);
+  args.call(this, Array.prototype.slice.call(arguments));
+  util.merge(opt, {'handler': WebSocketHandler});
 
-      bootstrap.setPipelineFactory(Pipeline(connectionListener));
-      bootstrap.bind(internalAddress);
-    }, cb).recover(function(e) {
-      java.lang.System.out.println(e)
-    });
-  };
-
-}
-
-exports.createServer = function(listener) {
-  return new Server(listener);
+  return new Server(opt, fn);
 };
+
