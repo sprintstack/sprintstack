b068bb359c931de60056f3c404ebab6ecf69bd81
Chris Mowforth <chris@mowforth.com>
2012-05-22 16:11:46 +0300

    initial time/timeEnd impl

--- resources/console.js
+++ resources/console.js
@@ -6,6 +6,20 @@ var Console = function() {
 
   logger = Dispatch.getSystem().log();
 
+  timer = new actor(function(msg) {
+    if (!this.timestamps) this.timestamps = {};
+    if (msg.constructor === String) {
+      this.timestamps[msg] = new Date().getTime();
+    java.lang.System.out.println(msg)
+    } else {
+      var id = this.timestamps[msg.end];
+      if (id) {
+        var endtime = new Date().getTime();
+        logger.info(msg.end + util.format(": %dms", endtime - id));
+      }
+    }
+  });
+
   this.log = function() {
     logger.info(util.format.apply(null, arguments));
   }
@@ -18,6 +32,14 @@ var Console = function() {
 
   this.error = function() {
     logger.error(util.format.apply(null, arguments));
+  }
+
+  this.time = function(id) {
+    timer.send(id);
+  }
+
+  this.timeEnd = function(id) {
+    timer.send({"end":id});
   }
 
 }
