ac0d497801f3a8ffd0f528b445924a29d1b34b92
Chris Mowforth <chris@mowforth.com>
2012-05-27 19:02:26 +0100

    implemented os.networkInterfaces()

--- resources/os.js
+++ resources/os.js
@@ -1,5 +1,6 @@
 importClass(java.lang.System);
 importClass(java.lang.Runtime)
+importClass(java.net.NetworkInterface);
 
 var OS = function() {
   this.hostname = function() {
@@ -24,6 +25,33 @@ var OS = function() {
 
   this.totalmem = function() {
     return Runtime.getRuntime().maxMemory();
+  }
+
+  this.networkInterfaces = function() {
+    var interfaces = NetworkInterface.getNetworkInterfaces();
+    var ifs = {};
+
+    while (interfaces.hasMoreElements()) {
+      var iface = interfaces.nextElement();
+      var addresses = iface.getInetAddresses();
+      var addrs = [];
+
+      while (addresses.hasMoreElements()) {
+        var addr = addresses.nextElement();
+        var ipfamily = addr.getHostAddress().indexOf(":") != -1 ? 'IPv6' : 'IPv4';
+        var info = {
+          address: new String(addr.getHostAddress()),
+          family: ipfamily,
+          internal: addr.isLoopbackAddress()
+        };
+        addrs.push(info);
+      }
+
+      ifs[iface.getName().toString()] = addrs;
+
+    }
+
+    return ifs;
   }
 };
 
