4cf7de749a926f48f21444deebaf7cf6c3ff507e
Chris Mowforth <chris@mowforth.com>
2012-12-08 18:02:59 +0000

    pre- FutureTask future commit

--- resources/future.js
+++ resources/future.js
@@ -1,92 +1,60 @@
-importClass(com.sprintstack.dispatch.Dispatch);
-importClass(com.sprintstack.dispatch.IFunction);
-importClass(Packages.akka.dispatch.Await);
-importClass(Packages.akka.dispatch.Filter);
-importClass(Packages.akka.dispatch.Futures);
-importClass(Packages.akka.dispatch.Mapper);
-importClass(Packages.akka.dispatch.OnComplete);
-importClass(Packages.akka.dispatch.Recover);
-importClass(Packages.akka.util.Duration);
-importClass(java.util.concurrent.Callable);
+importClass(java.util.concurrent.atomic.AtomicReference)
+importClass(java.util.concurrent.CountDownLatch)
+importClass(Packages.org.fusesource.hawtdispatch.Dispatch)
 
-var dispatcher = Dispatch.getSystem().dispatcher();
+var future = function(fn, auto) {
 
-var createFuture = function(work, cb) {
-  var c = new JavaAdapter(Callable, {call: work});
-  var future = Futures.future(c, dispatcher);
-  return future;
-}
+  var self = this;
 
-var future = function(f) {
-  if (f.constructor === Function) {
-    this.future = createFuture(f);
-  } else {
-    this.future = f
-  }
-
-  this.effect = function(fn) {
-    var callback = new JavaAdapter(OnComplete, {onComplete: fn});
-    var effector = this.future.andThen(callback);
-    return new future(effector);
-  }
+  this.val = new AtomicReference();
+  this.callback = new AtomicReference();
+  this.latch = new CountDownLatch(1);
 
   this.then = function(fn) {
-    var work = new JavaAdapter(Mapper, {apply: function(val) {
-      return createFuture(function() {
-        return fn(val);
-      });
-    }});
-    var mapped = this.future.flatMap(work);
-    return new future(mapped);
+    var f = new future(fn, false);
+    this.callback.set(f);
+    // If current future has finished already,
+    // invoke the next one right away.
+    if (this.isComplete()) f.start(this.val.get());
+    return f;
   }
 
-  this.check = function(fn) {
-    var predicate = new JavaAdapter(IFunction, {apply: function(val) {
-      return fn(val);
-    }});
-
-    var filtered = this.future.filter(Filter.filterOf(predicate));
-    return new future(filtered);
+  this.await = function() {
+    this.latch.await();
+    return this.val.get();
   }
 
-  this.recover = function(fn) {
-    var recovery = new JavaAdapter(Recover, {recover: function(err) {
-      return fn(err);
-    }});
-
-    var recoverable = this.future.recoverWith(recovery);
-    return new future(recoverable);
+  this.work = function(v) {
+    var val = fn(v);
+    self.val.set(val);
+    self.latch.countDown();
+    var cb = self.callback.get();
+    if (cb != null && !cb.isComplete())
+      cb.start(val);
   }
 
-  this.wait = function() {
-    try {
-      return Await.result(this.future, Duration.Inf());
-    } catch (e) {
-      if (e.toString().match("scala.MatchError") != null) {
-        return null;
-      }
-    }
-    return Await.result(this.future, Duration.Inf());
+  this.start = function(v) {
+    Dispatch.getGlobalQueue().execute(function() { self.work(v) });
   }
 
-  return this;
-};
+  this.isComplete = function() {
+    return (this.latch.getCount() == 0)
+  }
+
+  if (!auto) this.start();
+}
 
 future.compose = function() {
   var args = Array.prototype.slice.call(arguments);
   if (arguments[0].constructor === Array) args = arguments[0];
 
-  var futures = args.map(function(f) {
-    return f.future;
-  });
-
-  var composed = Futures.sequence(futures, dispatcher);
-  return new future(composed).then(function(v) {
-    var a = v.toArray().map(function(x) {
-      return x;
+  var f = new future(function(v) {
+    return args.map(function(f) {
+      return f.await();
     });
-    return a;
-  });
+  }, false);
+
+  return f;
 }
 
 module.exports = future;
