e481cdfdacc2f3c4c52b24cd89a6f02118d45b7b
Chris Mowforth <chris@mowforth.com>
2012-12-09 17:19:05 +0000

    warn about unimplemented console.time/timeEnd

--- resources/console.js
+++ resources/console.js
@@ -1,43 +1,29 @@
 importClass(java.lang.System);
 
 var util = require('util');
+var queue = require('queue');
 
-// Accepts log message in the format {"t":x,"p":y}
-// where t = type, p = payload
 var Logger = function() {
 
-  var internalLogger = new actor(function(msg) {
-    if (msg.t == "err") System.err.println(msg.p);
-    if (msg.t == "out") System.out.println(msg.p);
-  });
+  var outQueue = new queue();
+  var errQueue = new queue();
 
   this.out = function(msg) {
-    internalLogger.send({"t":"out","p":msg});
+    outQueue.push(function() {
+      System.out.println(msg);
+    });
   }
 
   this.err = function(msg) {
-    internalLogger.send({"t":"err","p":msg});
+    errQueue.push(function() {
+      System.err.println(msg);
+    });
   }
+};
 
-}
 
 var Console = function() {
   logger = new Logger();
-
-  timer = new actor(function(msg) {
-    if (msg.constructor === String) {
-      this.timestamps[msg] = new Date().getTime();
-    } else {
-      var id = this.timestamps[msg.end];
-      if (id) {
-        var endtime = new Date().getTime();
-        delete this.timestamps[msg.end];
-        logger.out(msg.end + util.format(": %dms", endtime - id));
-      }
-    }
-  }, {prestart: function() {
-    this.timestamps = {};
-  }});
 
   this.log = function() {
     var str = util.format.apply(null, arguments);
@@ -54,11 +40,11 @@ var Console = function() {
   this.error = this.warn;
 
   this.time = function(id) {
-    timer.send(id);
+    this.warn("time() not implemented!");
   }
 
   this.timeEnd = function(id) {
-    timer.send({"end":id});
+    this.warn("timeEnd() not implemented!");
   }
 
 }
