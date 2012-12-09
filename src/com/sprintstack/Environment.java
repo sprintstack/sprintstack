c7e4d8764d68add4f890efa2a5437ad30e1126a9
Chris Mowforth <chris@mowforth.com>
2012-12-09 02:38:23 +0000

    add timer + dispatcher singletons

--- src/com/sprintstack/Environment.java
+++ src/com/sprintstack/Environment.java
@@ -1,8 +1,14 @@
 package com.sprintstack;
 
 import java.nio.file.*;
+import java.util.concurrent.Executor;
+import org.jboss.netty.util.HashedWheelTimer;
+import org.fusesource.hawtdispatch.Dispatch;
 
 public class Environment {
+
+    private final static HashedWheelTimer systemTimer = new HashedWheelTimer();
+    private final static Executor globalExecutor = Dispatch.getGlobalQueue();
 
     public static String corePath() {
         return getJarPath().getParent().resolve("../resources/").normalize().toString();
@@ -15,5 +21,9 @@ public class Environment {
     private static Path getJarPath() {
         return Paths.get(SprintStack.class.getProtectionDomain().getCodeSource().getLocation().getPath());
     }
+
+    public static HashedWheelTimer getSystemTimer() { return systemTimer; }
+
+    public static Executor getDefaultDispatcher() { return globalExecutor; }
 
 }
