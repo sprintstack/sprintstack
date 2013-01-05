4242376e182cd2a6e4fff5e30cea06c328ca0c4c
Chris Mowforth <chris@mowforth.com>
2013-01-05 13:44:13 +0000

    remove Akka-specific event code

--- resources/events.js
+++ resources/events.js
@@ -1,20 +1,14 @@
 var EventEmitter = function() {
 
   this.on = function(event, listener) {
-    var work = new JavaAdapter(IFunction, {apply: listener});
-
-    var e = new EventHandler(event, work);
-    actor.tell(e);
   };
 
+  this.addListener = this.on;
+
   this.emit = function() {
-    var args = Array.prototype.slice.call(arguments);
-    var event = new Event(args.shift(), args.shift());
-    actor.tell(event);
   };
 
   this.stop = function() {
-    actor.tell(Actors.poisonPill());
   };
 
 };
